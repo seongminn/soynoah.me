9e723bdc79470e28d0d297d55e3c156b1106e698
seongminn <berry9901@gmail.com>
2024-04-17 23:31:03 +0900

    style: update header styles
    fix: remove max width on blur layer

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,19 +1,32 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import { site } from '~/configs/site';
+import { cn } from '~/libs/utils';
 
 import ThemeToggle from './theme-toggle';
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="header">
       <div className="container mx-auto grid h-full max-w-content grid-cols-[1fr,auto] items-center py-0">
         <nav className="mr-2 flex flex-1 items-center gap-4 pr-2">
-          <Link href="/" className="font-serif">
+          <Link href="/" className="font-serif font-semibold text-gray-700">
             {site.title}
           </Link>
           {site.menus.map(menu => (
-            <Link key={menu.path} href={menu.path} className="font-serif">
+            <Link
+              key={menu.path}
+              href={menu.path}
+              className={cn(
+                'text-nowrap font-serif text-gray-800 transition-opacity hover:opacity-80',
+                pathname.startsWith(menu.path) ? 'opacity-100' : 'opacity-60',
+              )}
+            >
               {menu.label}
             </Link>
           ))}
