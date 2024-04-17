89925e4df0803e9f23979aea3625a78dadbb79f2
seongminn <berry9901@gmail.com>
2024-04-17 22:40:37 +0900

    chore: change site meta data

--- configs/site.ts
+++ configs/site.ts
@@ -1,16 +1,16 @@
 export const site = {
-  title: `benieve`,
+  title: `comoluna`,
   description: `배운 것을 기록하고 생각을 공유하는 공간입니다.`,
-  url: `https://seongmin-v2.vercel.app/`,
+  url: `https://comoluna.vercel.app/`,
   author: {
     name: `Seongmin Choi`,
-    bio: '세상을 하얗게 뒤덮어버리는 눈처럼',
+    bio: '세상을 은은하게 비추는 달빛처럼,',
     contact: {
       email: `mailto:berry9901@gmail.com`,
       github: `https://github.com/seongminn`,
+      linkedin: `https://www.linkedin.com/in/%EC%84%B1%EB%AF%BC-%EC%B5%9C-4b8a57272/`,
       medium: ``,
       facebook: ``,
-      linkedin: ``,
       twitter: ``,
     },
   },
