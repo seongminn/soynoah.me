45d0e89f4e0cb6b1d7f2968e4f5b57b2b9b7155e
seongminn <berry9901@gmail.com>
2025-05-05 18:21:47 +0900

    fix: edit tailwindcss content path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: ['class'],
-  content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     fontFamily: {
       sans: ['var(--font-sans)', 'sans'],
