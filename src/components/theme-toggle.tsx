08b775a3fcf95b36041ad50b86b3d1e2f33ab272
seongminn <berry9901@gmail.com>
2025-05-08 22:52:29 +0900

    chore: minor change

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -9,21 +9,19 @@ export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div>
-      <IconButton
-        label="Toggle theme"
-        aria-live="polite"
-        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-      >
-        <Icons.sun
-          className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-          aria-hidden="true"
-        />
-        <Icons.moon
-          className="absolute box-border h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-          aria-hidden="true"
-        />
-      </IconButton>
-    </div>
+    <IconButton
+      label="Toggle theme"
+      aria-live="polite"
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      <Icons.sun
+        className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        aria-hidden="true"
+      />
+      <Icons.moon
+        className="absolute box-border h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        aria-hidden="true"
+      />
+    </IconButton>
   );
 }
