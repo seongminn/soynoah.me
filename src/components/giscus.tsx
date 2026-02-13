0b78c99c0686a4453b96d4c44cbd49c9600268c0
seongminn <berry9901@gmail.com>
2026-02-13 21:01:36 +0900

    fmt

--- src/components/giscus.tsx
+++ src/components/giscus.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { useTheme } from "next-themes";
-import type { HTMLAttributes } from "react";
-import { useEffect, useRef, useState } from "react";
+import { useTheme } from 'next-themes';
+import type { HTMLAttributes } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Giscus(props: HTMLAttributes<HTMLElement>) {
   const ref = useRef<HTMLIFrameElement>(null);
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme } = useTheme();
-  const theme = resolvedTheme === "dark" ? "noborder_dark" : "noborder_light";
+  const theme = resolvedTheme === 'dark' ? 'noborder_dark' : 'noborder_light';
 
   useEffect(() => {
     const iframe = ref.current;
@@ -16,23 +16,23 @@ export default function Giscus(props: HTMLAttributes<HTMLElement>) {
     if (!iframe) return;
 
     const giscusAttributes = {
-      src: "https://giscus.app/client.js",
-      "data-repo": "seongminn/soynoah.me",
-      "data-repo-id": "R_kgDOK8xLvQ",
-      "data-category": "Announcements",
-      "data-category-id": "DIC_kwDOK8xLvc4CcZnK",
-      "data-mapping": "pathname",
-      "data-strict": "0",
-      "data-reactions-enabled": "1",
-      "data-emit-metadata": "0",
-      "data-input-position": "bottom",
-      "data-theme": theme,
-      "data-lang": "ko",
-      crossorigin: "anonymous",
-      async: "true",
+      src: 'https://giscus.app/client.js',
+      'data-repo': 'seongminn/soynoah.me',
+      'data-repo-id': 'R_kgDOK8xLvQ',
+      'data-category': 'Announcements',
+      'data-category-id': 'DIC_kwDOK8xLvc4CcZnK',
+      'data-mapping': 'pathname',
+      'data-strict': '0',
+      'data-reactions-enabled': '1',
+      'data-emit-metadata': '0',
+      'data-input-position': 'bottom',
+      'data-theme': theme,
+      'data-lang': 'ko',
+      crossorigin: 'anonymous',
+      async: 'true',
     };
 
-    const giscusScript = document.createElement("script");
+    const giscusScript = document.createElement('script');
 
     Object.entries(giscusAttributes).forEach(([key, value]) => {
       giscusScript.setAttribute(key, value);
@@ -46,8 +46,8 @@ export default function Giscus(props: HTMLAttributes<HTMLElement>) {
   useEffect(() => {
     if (!mounted) return;
 
-    const iframe = document.querySelector<HTMLIFrameElement>("iframe.giscus-frame");
-    iframe?.contentWindow?.postMessage({ giscus: { setConfig: { theme } } }, "https://giscus.app");
+    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+    iframe?.contentWindow?.postMessage({ giscus: { setConfig: { theme } } }, 'https://giscus.app');
   }, [theme, mounted]);
 
   return <section {...props} ref={ref} />;
